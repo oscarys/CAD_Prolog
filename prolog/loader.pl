c2000fb5f3540a3456781a007a92ec8749434c1a
oscarys <oscar.yasu@gmail.com>
2026-05-15 00:38:07 -0600

    fix: Update loader.pl to handle headache and abdominal pain

--- prolog/loader.pl
+++ prolog/loader.pl
@@ -27,7 +27,7 @@
 % :- use_module('modules/jaundice',                []).
 % :- use_module('modules/haematemesis',            []).
 % :- use_module('modules/diarrhoea',               []).
-% :- use_module('modules/headache',                []).
+:- use_module('modules/headache',                []).
 % :- use_module('modules/convulsions',             []).
 % :- use_module('modules/coma_confusion',          []).
 % :- use_module('modules/pyrexia_unknown_origin',  []).
